b37cc02ef503ffb3784c285f3b4b6f24a55eb7cd
Hitansh <hitansh.gupta@kristalball.com>
2023-12-27 22:23:35 +0530

    role based authorization spring security

--- src/main/java/com/smart/smartcontactmanager/dao/UserRepository.java
+++ src/main/java/com/smart/smartcontactmanager/dao/UserRepository.java
@@ -2,7 +2,10 @@ package com.smart.smartcontactmanager.dao;
 
 import com.smart.smartcontactmanager.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
-
+    @Query("select u from User where u.email = : email ")
+    public User getUserByUserName(@Param("email") String email);
 }
